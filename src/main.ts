f946eb2e938356632622511d6dc75694186bcdf5
Rogue <rogue87.dev@gmail.com>
2024-08-23 03:54:45 +0200

    update LinkedList.ts
    man, implementing the insert function was a pain in the ass. I don't
    remember it was that hard to implement last time I did DSA in java.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
+import LinkedList from "./structures/LinkedList";
 import ArrayList from "./structures/ArrayList";
 
-const list = new ArrayList<number>([2, 5, 1, 8, 5, 9, 3, 1]);
-console.log(list.getData());
-list.sort((a, b) => a - b);
-console.log(list.getData());
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-console.log(list.get(3));
-list.set(3, 4);
-console.log(list.get(3));
+const arrayList = new ArrayList<number>([2, 5, 1, 8, 5, 9, 3, 1]);
+console.log(arrayList.getData());
+arrayList.sort((a, b) => a - b);
+console.log(arrayList.getData());
+
+console.log(arrayList.get(3));
+arrayList.set(3, 4);
+console.log(arrayList.get(3));
+
+const linkedList = new LinkedList<number>();
+for (let i = 0; i < 25; i++) {
+  linkedList.append(getRandomInt(1, 1000));
+}
+for (let i = 0; i < 25; i++) {
+  linkedList.prepend(getRandomInt(1, 1000));
+}
+
+for (let i = 1; i <= 10; i++) {
+  linkedList.append(i);
+}
+
+linkedList.insert(99, 9, "after");
+
+linkedList.forEach((data) => {
+  console.log(data);
+});
--- src/structures/LinkedList.ts
+++ src/structures/LinkedList.ts
@@ -11,8 +11,8 @@ class Node<T> {
 }
 
 export default class LinkedList<T> {
-  head: Node<T> | null = null;
-  tail: Node<T> | null = null;
+  private head: Node<T> | null = null;
+  private tail: Node<T> | null = null;
 
   append(data: T): void {
     const newNode = new Node(data);
@@ -38,7 +38,83 @@ export default class LinkedList<T> {
     }
   }
 
+  insert(data: T, index: number, placement?: "before" | "after"): void {
+    if (!Number.isInteger(index) || index < 0) {
+      throw new Error("Index must be a non-negative integer.");
+    }
 
+    const newNode = new Node<T>(data);
+
+    if (index === 0) {
+      if (this.head) {
+        switch (placement) {
+          case "before":
+            this.prepend(data);
+            break;
+          case "after":
+            if (this.head.next) {
+              newNode.next = this.head.next;
+              this.head.next.prev = newNode;
+              this.head.next = newNode;
+              newNode.prev = this.head;
+            } else this.append(data);
+            break;
+          default:
+            this.prepend(data);
+            break;
+        }
+      } else {
+        this.head = newNode;
+        this.tail = newNode;
+      }
+    } else {
+      let leader: Node<T> | null = this.head;
+      let count = 0;
+      while (leader) {
+        if (count === index) {
+          switch (placement) {
+            case "before":
+              newNode.next = leader;
+              newNode.prev = leader.prev;
+              leader.prev!.next = newNode;
+              leader.prev = newNode;
+              break;
+            case "after":
+              if (leader === this.tail) {
+                this.append(data);
+              } else {
+                newNode.prev = leader;
+                newNode.next = leader.next;
+                leader.next!.prev = newNode;
+                leader.next = newNode;
+              }
+              break;
+            default:
+              newNode.next = leader;
+              newNode.prev = leader.prev;
+              leader.prev!.next = newNode;
+              leader.prev = newNode;
+              break;
+          }
+          return;
+        } else {
+          count++;
+          leader = leader.next;
+        }
+      }
+      if (index >= count) {
+        this.append(data);
+      }
+    }
+  }
+
+  forEach(callback: (data: T) => void): void {
+    let currentPtr = this.head;
+    while (currentPtr) {
+      callback(currentPtr.data);
+      currentPtr = currentPtr.next;
+    }
+  }
 
   constructor();
   constructor(node: Node<T>);
